5cfdd09a4985aa9fe474f9cb551d881ec260c27e
ManuelCM21 <manuelchuncamamani@gmail.com>
2023-04-03 12:53:11 -0500

    WebSocket con SpringBoot

--- Sesion 3 - Socket y Websocket/websocket/src/main/java/com/example/websocket/config/WebSocketConfig.java
+++ Sesion 3 - Socket y Websocket/websocket/src/main/java/com/example/websocket/config/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 }
